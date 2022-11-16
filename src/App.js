817816077bc9a2d16e0b3ec123ce645037f8cd9a
N-kapila <nirmalkapilarathne1998@gmail.com>
2022-11-16 19:47:42 +0530

    add task

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ import "./App.css";
 function App() {
   //ToDo list
   const [toDo, setToDo] = useState([
-    // { id: 1, title: "Task 1", status: false },
-    // { id: 2, title: "Task 2", status: false },
+    { id: 1, title: "Task 1", status: false },
+    { id: 2, title: "Task 2", status: false },
   ]);
 
   //Temp State
@@ -20,7 +20,14 @@ function App() {
   const [updateData, setUpdateData] = useState("");
 
   //Add task
-  const addTask = () => {};
+  const addTask = () => {
+    if (newTask) {
+      let num = toDo.length + 1;
+      let newEntry = { id: num, title: newTask, status: false };
+      setToDo([...toDo, newEntry]);
+      setNewTask("");
+    }
+  };
 
   //Delete task
   const deleteTask = (id) => {};
@@ -39,9 +46,69 @@ function App() {
 
   return (
     <div className="container App">
-      <h2>To Do List App</h2>
+      <br></br>
+      <h2>To Do List </h2>
+      <br></br>
 
+      {/* Update Task */}
+      <div className="row">
+        <div className="col">
+          <input className="form-control form-control-lg" />
+        </div>
+        <div className="col-auto">
+          <button className="btn btn-lg btn-success mr-20"> Update</button>
+          <button className="btn btn-lg btn-warning mr-20"> Cancel</button>
+        </div>
+      </div>
+      <br></br>
+
+      {/*Add Task*/}
+      <div className="row">
+        <div className="col">
+          <input
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+            className="form-control form-control-lg"
+          />
+        </div>
+        <div className="col-auto">
+          <button onClick={addTask} className="btn btn-lg btn-success">
+            {" "}
+            Add Task
+          </button>
+        </div>
+      </div>
+      <br></br>
+
+      {/*Display ToDos*/}
       {toDo && toDo.length ? "" : "No tasks..."}
+
+      {toDo &&
+        toDo
+          .sort((a, b) => (a.id > b.id ? 1 : -1))
+          .map((task, index) => {
+            return (
+              <React.Fragment key={task.id}>
+                <div className="col taskBg">
+                  <div className={task.status ? "done" : ""}>
+                    <span className="taskNumber">{index + 1}</span>
+                    <span className="taskText">{task.title}</span>
+                  </div>
+                  <div className="iconsWrap">
+                    <span title="Completed / Not Completed">
+                      <FontAwesomeIcon icon={faCircleCheck} />
+                    </span>
+                    <span title="Edit">
+                      <FontAwesomeIcon icon={faPen} />
+                    </span>
+                    <span title="Delete">
+                      <FontAwesomeIcon icon={faTrashCan} />
+                    </span>
+                  </div>
+                </div>
+              </React.Fragment>
+            );
+          })}
     </div>
   );
 }
